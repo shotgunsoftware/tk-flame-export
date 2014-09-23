bd6cd6eb19c0f916b03d34137df85ff27ae358fd
Manne Öhrström <manne@shotgunsoftware.com>
2014-09-23 11:28:45 +0100

    Added std scaffold.

---
+++ app.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+"""
+Flame content exporter.
+"""
+
+import sys
+import os
+import platform
+
+from sgtk.platform import Application
+
+class FlameExport(Application):
+    
+    def init_app(self):
+        """
+        Called as the application is being initialized
+        """
+        self.log_debug("%s: Initializing" % self)
+
