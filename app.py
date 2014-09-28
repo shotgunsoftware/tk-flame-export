fbb9db06c22e0412cf8e6722f8e6e0713adc84d2
Manne Öhrström <manne@shotgunsoftware.com>
2014-09-29 00:21:10 +0100

    Added more structure, comments etc.

--- app.py
+++ app.py
@@ -13,24 +13,122 @@ Flame content exporter.
 """
 
 import sys
+import uuid
 import os
 import platform
 
 from sgtk.platform import Application
 
+tk_flame_export = self.import_module("tk_flame_export")
+
+
 class FlameExport(Application):
+    """
+    Export functionality to automate and streamline content export out of Flame.
+    """
     
     def init_app(self):
         """
-        Called as the application is being initialized
+        Called as the application is being initialized.
         """
         self.log_debug("%s: Initializing" % self)
-
-    def get_export_presents(self):
+        self._sessions = {}        
+        
+    def get_export_presets(self):
         """
-        Placeholder
+        Returns the different presets defined in the configuration.
+        
+        :returns: List of tuples - (preset title, preset name)
         """
-        return ["Shotgun Sequence Export", "Shotgun Other Sequence Export"]
+        presets = []
+        presets.append(("Shotgun Sequence Export", "sequence_export"))
+        presets.append(("Shotgun Sequence Export 2", "sequence_export_2"))
+        return presets
     
+    def create_export_session(self, preset):
+        """
+        Start a new export session.
+        Creates a session object which represents a single export session in flame.
+        
+        :returns: session id string which is later passed into various methods
+        """
+        session_id = "tk_%s" % uuid.uuid4().hex
+        self._sessions[session_id] = tk_flame_export.ExportSession(preset)
+        return session_id
     
+    def _resolve_session(self, session_id):
+        """
+        Helper method which validates and reuturns a session id
+        
+        :param session_id: Export session id, as created by create_export_session
+        :returns: An ExportSession object
+        """
+        if session_id not in self._sessions:
+            raise TankError("Export session %s not associated with app %s!" % (session_id, self))
+        return self._sessions[session_id]
+    
+    def get_destination_host(self, session_id):
+        """
+        Returns the host to which the export is routed to.
+        
+        :param session_id: String which identifies which export session is being referred to
+        :returns: host for export
+        """
+        # hard coded to localhost for now
+        return "localhost"
+    
+    def get_destination_path(self, session_id):
+        """
+        Returns the base path for the export 
+        
+        :param session_id: String which identifies which export session is being referred to
+        :returns: full path string
+        """        
+        return self._resolve_session(session_id).get_destination_path()
+        
+    def get_export_preset_path(self, session_id):
+        """
+        Returns a path to an xml export profile 
+        
+        :param session_id: String which identifies which export session is being referred to
+        :return: full path string
+        """
+        return self._resolve_session(session_id).get_export_preset_path()
+        
+    def prepare_export_structure(self, session_id, sequence_name, shot_names):
+        """
+        Called from the flame hooks before export.
+        This is the time to set up the structure in Shotgun.
+        
+        :param session_id: String which identifies which export session is being referred to
+        :param sequence_name: The sequence that is being exported
+        :param shot_name: list of shots to be exported
+        """
+        return self._resolve_session(session_id).prepare_export_structure()        
+        
+    def use_backburner_post_events(self):
+        """
+        Controls if events should be routed via the render farm
+        
+        :returns: true if renderfarm is used, false if not
+        """
+        return False
+        
+    def register_publish(self, session_id, info):
+        """
+        Called when an item has been exported
+        
+        :param session_id: String which identifies which export session is being referred to
+        :param info: metadata dictionary for the publish
+        """        
+        return self._resolve_session(session_id).register_publish(info)
+        
+    def post_process_export(self, session_id):
+        """
+        Called when an export has completed
+        
+        :param session_id: String which identifies which export session is being referred to
+        """
+        return self._resolve_session(session_id).post_process_export()
+        
         
--- flame_hooks/exportHook.py
+++ flame_hooks/exportHook.py
@@ -8,352 +8,227 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+def preCustomExport(info, userData):
+    """
+    Hook called before a custom export begins. The export will be blocked
+    until this function returns. This can be used to fill information that would
+    have normally been extracted from the export window.
+    
+    :param info: Modifiable dictionary with info about the export. Contains the keys
+                 - destinationHost: Host name where the exported files will be written to.
+                 - destinationPath: Export path root.
+                 - presetPath: Path to the preset used for the export.
+    
+    :param userData: Dictionary that could have been populated by previous export hooks and that
+                     will be carried over into the subsequent export hooks.
+                     This can be used by the hook to pass black box data around.
+    """
+    # first, get the toolkit app handle
+    import sgtk
+    e = sgtk.platform.current_engine()
+    app = e.apps["tk-flame-export"]
 
+    # get the preset that the user selected
+    current_preset = userData["preset_name"]
 
+    # create a session object in the app - this is how 
+    # we keep track of what is going on
+    session_id = app.create_export_session(current_preset)
+    userData["session_id"] = session_id
 
+    # --- at this point we could potentially pop up a UI
+    # --- to gather additional parameters from the user
 
-
-
-
-# Hook called before a custom export begins. The export will be blocked
-# until this function returns. This can be used to fill information that would
-# have normally been extracted from the export window.
-#
-# info [Dictionary] [Modifiable]
-#    Information about the export,
-#
-#    Keys:
-#
-#    destinationHost: [String] [Modifiable]
-#       Host name where the exported files will be written to.
-#       Defaults to localhost.
-#
-#    destinationPath: [String] [Modifiable]
-#       Export path root.
-#       Defaults to /tmp
-#
-#    presetPath: [String] [Modifiable]
-#       Path to the preset used for the export.
-#       Must be defined by this method.
-#
-# userData [Dictionary] [Modifiable]
-#   Dictionary that could have been populated by previous export hooks and that
-#   will be carried over into the subsequent export hooks.
-#   This can be used by the hook to pass black box data around.
- 
-def preCustomExport( info, userData ):
-   pass
- 
- 
-# Hook called after a custom export ends. The export will be blocked
-# until this function returns.
-#
-# info [Dictionary] [Modifiable]
-#    Information about the export,
-#
-#    Keys:
-#
-#    destinationHost: [String]
-#       Host name where the exported files were written to.
-#
-#    destinationPath: [String]
-#       Export path root.
-#
-#    presetPath: [String]
-#       Path to the preset used for the export.
-#
-# userData [Dictionary] [Modifiable]
-#   Dictionary that could have been populated by previous export hooks and that
-#   will be carried over into the subsequent export hooks.
-#   This can be used by the hook to pass black box data around.
- 
-def postCustomExport( info, userData ):
-   pass
-
-
-# Hook called before an export begins. The export will be blocked
-# until this function returns.
-#
-# info [Dictionary] [Modifiable]
-#    Information about the export,
-#
-#    Keys:
-#
-#    destinationHost: [String]
-#       Host name where the exported files will be written to.
-#
-#    destinationPath: [String]
-#       Export path root.
-#
-# userData [Dictionary] [Modifiable]
-#   Dictionary that could have been populated by previous export hooks and that
-#   will be carried over into the subsequent export hooks.
-#   This can be used by the hook to pass black box data around.
-def postExport( info, userData ):
-   pass
- 
- 
-
-# Hook called after an export ends. The export will be blocked
-# until this function returns.
-#
-# info [Dictionary] [Modifiable]
-#    Information about the export,
-#
-#    Keys:
-#
-#    destinationHost: [String]
-#       Host name where the exported files were written to.
-#
-#    destinationPath: [String]
-#       Export path root.
-#
-# userData [Dictionary] [Modifiable]
-#   Dictionary that could have been populated by previous export hooks.
-#   This can be used by the hook to pass black box data around.
- 
-def postExport( info, userData ):
-   pass
-
-
-# Hook called before a sequence export begins. The export will be blocked
-# until this function returns.
-#
-# info [Dictionary] [Modifiable]
-#    Information about the export,
-#
-#    Keys:
-#
-#    destinationHost: [String]
-#       Host name where the exported files will be written to.
-#
-#    destinationPath: [String]
-#       Export path root.
-#
-#    sequenceName: [String]
-#       Name of the exported sequence.
-#
-#    shotNames: [String]
-#       Tuple of all shot names in the exported sequence. Multiple segments
-#       could have the same shot name.
-#
-# userData [Dictionary] [Modifiable]
-#   Dictionary that could have been populated by previous export hooks and that
-#   will be carried over into the subsequent export hooks.
-#   This can be used by the hook to pass black box data around.
- 
-def preExportSequence( info, userData ):
-   pass
-
-
-# Hook called after a sequence export ends. The export will be blocked
-# until this function returns.
-#
-# info [Dictionary] [Modifiable]
-#    Information about the export,
-#
-#    Keys:
-#
-#    destinationHost: [String]
-#       Host name where the exported files were written to.
-#
-#    destinationPath: [String]
-#       Export path root.
-#
-#    sequenceName: [String]
-#       Name of the exported sequence.
-#
-#    shotNames: [String]
-#       Tuple of all shot names in the exported sequence. Multiple segment
-#       could have the same shot name.
-#
-# userData [Dictionary] [Modifiable]
-#   Dictionary that could have been populated by previous export hooks and that
-#   will be carried over into the subsequent export hooks.
-#   This can be used by the hook to pass black box data around.
- 
-def postExportSequence( info, userData ):
-   pass
- 
- 
-# Hook called before an asset export begins. The export will be blocked
-# until this function returns.
-#
-# info [Dictionary] [Modifiable]
-#    Information about the export,
-#
-#    Keys:
-#
-#    destinationHost: [String]
-#       Host name where the exported files will be written to.
-#
-#    destinationPath: [String]
-#       Export path root.
-#
-#    namePattern: [String]
-#       List of optional naming tokens.
-#
-#    resolvedPath: [String]
-#       Full file pattern that will be exported with all the tokens resolved.
-#
-#    name: [String]
-#       Name of the exported asset.
-#
-#    sequenceName: [String]
-#       Name of the sequence the asset is part of.
-#
-#    shotName: [String]
-#       Name of the shot the asset is part of.
-#
-#    assetType: [String]
-#       Type of exported asset.
-#       ( 'video', 'audio', 'batch', 'openClip', 'batchOpenClip' )
-#
-#    width: [Long]
-#       Frame width of the exported asset.
-#
-#    height: [Long]
-#       Frame height of the exported asset.
-#
-#    aspectRatio: [Double]
-#       Frame aspect ratio of the exported asset.
-#
-#    depth: [String]
-#       Frame depth of the exported asset.
-#       ( '8-bits', '10-bits', '12-bits', '16 fp' )
-#
-#    scanFormat: [String]
-#       Scan format of the exported asset.
-#       ( 'FILED_1', 'FIELD_2', 'PROGRESSIVE' )
-#
-#    fps: [Double]
-#       Frame rate of exported asset.
-#
-#    versionName: [String]
-#       Current version name of export (Empty if unversioned).
-#
-#    versionNumber: [Integer]
-#       Current version number of export (0 if unversioned).
-#
-# userData [Dictionary] [Modifiable]
-#   Dictionary that could have been populated by previous export hooks and that
-#   will be carried over into the subsequent export hooks.
-#   This can be used by the hook to pass black box data around.
- 
-def preExportAsset( info, userData ):
-   pass
- 
-# Hook called after an asset export ends. The export will be blocked
-# until this function returns.
-#
-# info [Dictionary] [Modifiable]
-#    Information about the export,
-#
-#    Keys:
-#
-#    destinationHost: [String]
-#       Host name where the exported files were written to.
-#
-#    destinationPath: [String]
-#       Export path root.
-#
-#    namePattern: [String]
-#       List of optional naming tokens.
-#
-#    resolvedPath: [String]
-#       Full file pattern that will be exported with all the tokens resolved.
-#
-#    name: [String]
-#       Name of the exported asset.
-#
-#    sequenceName: [String]
-#       Name of the sequence the asset is part of.
-#
-#    shotName: [String]
-#       Name of the shot the asset is part of.
-#
-#    assetType: [String]
-#       Type of exported asset.
-#       ( 'video', 'audio', 'batch', 'openClip', 'batchOpenClip' )
-#
-#    isBackground: [Boolean]
-#       True if the export of the asset happened in the background.
-#
-#    backgroundJobId: [String]
-#       Id of the background job given by the backburner manager upon
-#       submission. Empty if job is done in foreground.
-#
-#    width: [Long]
-#       Frame width of the exported asset.
-#
-#    height: [Long]
-#       Frame height of the exported asset.
-#
-#    aspectRatio: [Double]
-#       Frame aspect ratio of the exported asset.
-#
-#    depth: [String]
-#       Frame depth of the exported asset.
-#       ( '8-bits', '10-bits', '12-bits', '16 fp' )
-#
-#    scanFormat: [String]
-#       Scan format of the exported asset.
-#       ( 'FILED_1', 'FIELD_2', 'PROGRESSIVE' )
-#
-#    fps: [Double]
-#       Frame rate of exported asset.
-#
-#    versionName: [String]
-#       Current version name of export (Empty if unversioned).
-#
-#    versionNumber: [Integer]
-#       Current version number of export (0 if unversioned).
-#
-# userData [Dictionary] [Modifiable]
-#   Dictionary that could have been populated by previous export hooks and that
-#   will be carried over into the subsequent export hooks.
-#   This can be used by the hook to pass black box data around.
- 
-def postExportAsset( info, userData ):
-   pass
+    # populate export settings
+    info["destinationHost"] = app.get_destination_host(session_id)
+    info["destinationPath"] = app.get_destination_path(session_id)
+    info["presetPath"] = app.get_export_preset_path(session_id)
    
  
+def preExportSequence(info, userData):
+    """
+    Hook called before a sequence export begins. The export will be blocked
+    until this function returns.
+    
+    :param info: Information about the export. Contains the keys      
+                 - destinationHost: Host name where the exported files will be written to.
+                 - destinationPath: Export path root.
+                 - sequenceName: Name of the exported sequence.
+                 - shotNames: Tuple of all shot names in the exported sequence. 
+                              Multiple segments could have the same shot name.
+    
+    :param userData: Dictionary that could have been populated by previous export hooks and that
+                     will be carried over into the subsequent export hooks.
+                     This can be used by the hook to pass black box data around.    
+    """
+    
+    # Example of output coming from flame:
+    # 
+    # [PYTHON HOOK] Calling python hook function preExportSequence(
+    # {'destinationHost': 'Mannes-MacBook-Pro-2.local', 
+    #  'destinationPath': '/tmp', 
+    #  'sequenceName': 'X-Ball Gladiator 3', 
+    #  'shotNames': ('sh_0010', 'sh_0020', 'sh_0030', 'sh_10', 'sh_20', 'sh_30', 'sh_40')}, 
+    # {'preset_name': 'sequence_export_2', 'session_id': '123'})
+   
+    # first, get the toolkit app handle
+    import sgtk
+    e = sgtk.platform.current_engine()
+    app = e.apps["tk-flame-export"]
+
+    # get the preset that the user selected
+    # note that in the case of a non-tk export, there
+    # may not be a session id.    
+    session_id = userData.get("session_id")
+    if session_id:
+        # before the export happens call out to the 
+        # toolkit export app to set up folder structure on disk
+        app.prepare_export_structure(session_id, info["sequenceName"], info["shotNames"])
+
  
+def postExportAsset(info, userData):
+    """    
+    Hook called after an asset export ends. The export will be blocked
+    until this function returns.
+    
+    :param info: Dictionary with a number of parameters:
+    
+       destinationHost: Host name where the exported files will be written to.
+       destinationPath: Export path root.
+       namePattern:     List of optional naming tokens.
+       resolvedPath:    Full file pattern that will be exported with all the tokens resolved.
+       name:            Name of the exported asset.
+       sequenceName:    Name of the sequence the asset is part of.
+       shotName:        Name of the shot the asset is part of.
+       assetType:       Type of exported asset. ( 'video', 'audio', 'batch', 'openClip', 'batchOpenClip' )
+       isBackground:    True if the export of the asset happened in the background.
+       backgroundJobId: Id of the background job given by the backburner manager upon submission. 
+                        Empty if job is done in foreground.
+       width:           Frame width of the exported asset.
+       height:          Frame height of the exported asset.
+       aspectRatio:     Frame aspect ratio of the exported asset.
+       depth:           Frame depth of the exported asset. ( '8-bits', '10-bits', '12-bits', '16 fp' )
+       scanFormat:      Scan format of the exported asset. ( 'FILED_1', 'FIELD_2', 'PROGRESSIVE' )
+       fps:             Frame rate of exported asset.
+       versionName:     Current version name of export (Empty if unversioned).
+       versionNumber:   Current version number of export (0 if unversioned).
+    
+    :param userData: Dictionary that could have been populated by previous export hooks and that
+                     will be carried over into the subsequent export hooks.
+                     This can be used by the hook to pass black box data around.
+    """
+    
+    # Examples of output: 
+    #    
+    # [PYTHON HOOK] Calling python hook function postExportAsset(
+    # {'height': 1080L, 
+    # 'destinationPath': '/Volumes/G-DRIVE mobile/tmp/', 
+    # 'destinationHost': 'Mannes-MacBook-Pro-2.local', 
+    # 'sourceOut': 432L, 
+    # 'assetType': 'video', 
+    # 'recordIn': 0L, 
+    # 'sequenceFps': '23.976', 
+    # 'width': 1920L, 
+    # 'fps': '23.976', 
+    # 'resolvedPath': 'X-Ball Gladiator 3/IMPORT/1920x1080/s0014.[00000430-00000431].dpx', 
+    # 'sequenceName': 'X-Ball Gladiator 3', 
+    # 'shotName': 'sh_0010',
+    # 'assetName': '010_Jump_Sand', 
+    # 'versionNumber': 0L, 
+    # 'versionName': 'v<version>', 
+    # 'recordOut': 2L, 
+    # 'sourceIn': 430L, 
+    # 'scanFormat': 'PROGRESSIVE', 
+    # 'namePattern': '<name>/<tape>/<width>x<height>/s<segment>.<timecode><ext>', 
+    # 'depth': '8-bit', 
+    # 'isBackground': False, 
+    # 'aspectRatio': 1.7777777910232544}, {})
+    
+    # first, get the toolkit app handle
+    import sgtk
+    e = sgtk.platform.current_engine()
+    app = e.apps["tk-flame-export"]
+
+    # get the preset that the user selected
+    # note that in the case of a non-tk export, there
+    # may not be a session id.    
+    session_id = userData.get("session_id")
+    if session_id:
+        # tell the toolkit export app about this file
+        app.register_publish(session_id, info)   
+   
+
  
- 
-# Indicates whether postExportAsset should be called from a backburner job or
-# directly from the application.
-#
-# Warning: Not generating a postExportAsset backburner job for exports that are
-# using backburner could result in postExportAsset being called before the
-# export job is complete.
- 
+def postCustomExport(info, userData):
+    """
+    Hook called after a custom export ends. The export will be blocked
+    until this function returns.
+    
+    :param info: Information about the export. Contains the keys      
+                 - destinationHost: Host name where the exported files will be written to.
+                 - destinationPath: Export path root.
+                 - presetPath: Path to the preset used for the export.
+    
+    :param userData: Dictionary that could have been populated by previous export hooks and that
+                     will be carried over into the subsequent export hooks.
+                     This can be used by the hook to pass black box data around.
+    """
+
+    # Example input:
+    #
+    # [PYTHON HOOK] Calling python hook function postCustomExport(
+    # {'destinationHost': 'localhost', 
+    #  'destinationPath': '/tmp', 
+    #  'presetPath': '/usr/discreet/flameassist_2015.2.pr99/export/presets/file_sequence/Jpeg (8-bit).xml'}, 
+    # {'preset_name': 'sequence_export_2'})
+
+    # first, get the toolkit app handle
+    import sgtk
+    e = sgtk.platform.current_engine()
+    app = e.apps["tk-flame-export"]
+
+    # get the preset that the user selected
+    # note that in the case of a non-tk export, there
+    # may not be a session id.
+    session_id = userData.get("session_id")
+    if session_id:
+        app.post_process_export(session_id)
+
+def getCustomExportProfiles(profiles):
+    """
+    Hook returning the custom export profiles to display to the user in the
+    contextual menu.
+
+    :param profiles: A dictionary of userData dictionaries where 
+                     the keys are the name of the profiles to show in contextual menus.
+    """
+    import sgtk
+    e = sgtk.platform.current_engine()
+    app = e.apps["tk-flame-export"]
+    
+    for (preset_title, preset_name) in app.get_export_presets(): 
+        profiles[preset_title] = {"preset_name": preset_name}
+
 def useBackburnerPostExportAsset():
-   return True
- 
- 
+    """
+    Indicates whether postExportAsset should be called from a backburner job or
+    directly from the application.
+    
+    Warning: Not generating a postExportAsset backburner job for exports that are
+    using backburner could result in postExportAsset being called before the
+    export job is complete.
+    
+    :returns: True to generate post export assets events on backburner, false if not
+    """
 
- 
-# Hook returning the custom export profiles to display to the user in the
-# contextual menu.
-#
-# profiles [Dictionary] [Modifiable]
-#
-#    A dictionary of userData dictionaries where the keys are the name
-#    of the profiles to show in contextual menus.
- 
-def getCustomExportProfiles( profiles ):
-   
-   import sgtk
-   e = sgtk.platform.current_engine()
-   app = e.apps["tk-flame-export"]
-   
-   for preset in app.get_export_presents(): 
-       profiles[preset] = {"foo": "bar"}
+    # first, get the toolkit app handle
+    import sgtk
+    e = sgtk.platform.current_engine()
+    app = e.apps["tk-flame-export"]
 
-
-
-
-
-
-
+    # get the preset that the user selected
+    # todo - include presets?
+    #current_preset = userData["preset_name"]
+    
+    # populate export settings
+    return app.use_backburner_post_events()
